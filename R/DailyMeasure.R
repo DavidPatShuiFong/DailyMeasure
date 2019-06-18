beaf9b66497e74655089a23489a6e364632772bb
David Fong <vkelim@bigpond.com>
2019-06-18 21:46:01 +1000

    add 'appmode' without navigation/addressbar
    also allow setting of launch.browser

--- R/DailyMeasure.R
+++ R/DailyMeasure.R
@@ -2,16 +2,28 @@
 #' (C) David Fong, 2019
 #'
 #' launches the DailyMeasure app
+#'
+#' @param appmode set to "chrome" if wanting to launch chrome in app mode
+#'  app-mode has not forward/back buttons, tabs, or address-bar
+#' @param launch.browser=TRUE normally launches a web-browser. within
+#'  Rstudio, launch.browser=FALSE will start a RStudio browser.
+#'
 #' @return None
 #'
 #' @import shiny tidyverse dbplyr
 #' @importFrom shinydashboard menuItem tabItems tabItem
 #'
 #' @export
-DailyMeasure <- function () {
+DailyMeasure <- function (appmode = "", launch.browser = TRUE) {
+
+  if (appmode == "chrome") {
+    launch.browser = function(shinyurl) {
+      shell(paste0("start chrome --app=", shinyurl))
+    }
+  }
 
   ##### Run the application ###########################################
   shiny::shinyApp(ui = DailyMeasureUI(), server = DailyMeasureServer,
-                  options = list(launch.browser = TRUE))
+                  options = list(launch.browser = launch.browser))
 
 }

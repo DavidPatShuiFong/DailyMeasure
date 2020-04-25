d3ef14a125b4b24090e45cb2eec87360c132aed4
David Fong <vkelim@bigpond.com>
2020-04-26 00:22:38 +1000

    COVID-19 bulk billing incentive prompt option in billing view

--- R/module_Billings.R
+++ R/module_Billings.R
@@ -25,8 +25,12 @@ billings_datatableUI <- function(id) {
           width = "20em"
         )
       ),
+      shiny::column(2,
+        offset = 2,
+        shiny::uiOutput(ns("billing_options"))
+      ),
       shiny::column(3,
-        offset = 5, # note that total 'column' width = 12
+        offset = 1, # note that total 'column' width = 12
         shinyWidgets::switchInput(
           inputId = ns("allbillings_view"),
           label = paste("Show all day's billings"),
@@ -61,6 +65,26 @@ billings_datatableUI <- function(id) {
 billings_datatable <- function(input, output, session, dMBillings) {
   ns <- session$ns
 
+  output$billing_options <-  shiny::renderUI({
+    shinyWidgets::dropdown(
+      input_id = ns("billing_options_dropdown"),
+      icon = icon("gear"),
+      label = "Inclusions/Exclusions",
+      shinyWidgets::checkboxGroupButtons(
+        inputId = ns("billing_options_chosen"),
+        label = "Billing Options",
+        choices = c("COVID-19 Bulk Billing Incentive"),
+        selected = c("COVID-19 Bulk Billing Incentive"),
+        # initially all chosen, which includes choices to
+        #  'include' ATSI 35-44 years,
+        # and 'exclude'
+        #  those with known cardiovascular disease and age 75
+        status = "primary",
+        checkIcon = list(yes = icon("ok", lib = "glyphicon"))
+      )
+    )
+  })
+
   billings_list <- shiny::eventReactive(
     c(
       dMBillings$billings_listR(),
@@ -80,7 +104,9 @@ billings_datatable <- function(input, output, session, dMBillings) {
       billingslist <- dMBillings$list_billings(
         lazy = TRUE,
         screentag = !input$printcopy_view,
-        screentag_print = input$printcopy_view
+        screentag_print = input$printcopy_view,
+        rawbilling = TRUE
+        # the 'raw' billings will be in the 'billings' column
       )
 
       return(billingslist)
@@ -91,14 +117,132 @@ billings_datatable <- function(input, output, session, dMBillings) {
     dMBillings$own_billings <- !input$allbillings_view
   })
 
+  check_for_covid19_bulkbilling <- function(InternalID, DOB, Date, Age, billings) {
+
+    if (is.null(billings)) {
+      return(as.character(NA))
+      # wasn't billed, so can't add a bulk-billing incentive!
+    }
+
+    if (any(c(10990, 10991, 10981, 10982) %in% billings)) {
+      return(as.character(NA))
+      # already charged a bulk-billing incentive
+    }
+
+    if (Age >= 70) {
+      return("Age 70+")
+    }
+    intID_Date <- data.frame(InternalID = InternalID, Date = Date)
+    if (Age >= 50 &&
+        length(dMBillings$dM$atsi_list(intID_Date))) {
+      return("ATSI 50+")
+    }
+    if (length(dMBillings$dM$pregnant_list(intID_Date))) {
+      return("Pregnant")
+    }
+    if (nrow(dMBillings$dM$postnatal_list(intID_Date,
+      include_edc = TRUE, # 'guess' delivery of no known result
+      days_min = 0, days_max = 365,
+      outcome = c(0,1)))) { # unknown result or live birth
+      # $postnatal_list returns a dataframe, not a vector
+      return("Parent of child less than 12 months")
+    }
+    if (length(dMBillings$dM$diabetes_list(intID_Date))) {
+      return("Diabetes")
+    }
+    if (length(dMBillings$dM$asthma_list(intID_Date))) {
+      return("Asthma")
+    }
+    if (length(dMBillings$dM$hiv_list(intID_Date))) {
+      return("HIV")
+    }
+    if (length(dMBillings$dM$malignancy_list(intID_Date))) {
+      return("Malignancy")
+    }
+    if (length(dMBillings$dM$haemoglobinopathy_list(intID_Date))) {
+      return("Haemoglobinopathy")
+    }
+    if (length(dMBillings$dM$asplenic_list(intID_Date))) {
+      return("Asplenia")
+    }
+    if (length(dMBillings$dM$transplant_list(intID_Date))) {
+      return("Transplant recipient")
+    }
+    if (length(dMBillings$dM$cardiacdisease_list(intID_Date))) {
+      return("Heart disease")
+    }
+    if (length(dMBillings$dM$chroniclungdisease_list(intID_Date))) {
+      return("Chronic lung disease")
+    }
+    if (length(dMBillings$dM$chronicliverdisease_list(intID_Date))) {
+      return("Chronic liver disease")
+    }
+    if (length(dMBillings$dM$neurologic_list(intID_Date))) {
+      return("Neurological disease")
+    }
+    if (length(dMBillings$dM$chronicrenaldisease_list(intID_Date))) {
+      return("Chronic renal disease")
+    }
+    if (length(dMBillings$dM$bmi30_list(intID_Date))) {
+      return("BMI>30")
+    }
+
+    # no conditions found
+    return(as.character(NA))
+  }
+
   styled_billings_list <- shiny::reactive({
     shiny::validate(
       shiny::need(billings_list(), "No appointments in selected range")
     )
 
+    billings <- billings_list()
+
+    if ("COVID-19 Bulk Billing Incentive" %in% input$billing_options_chosen) {
+      billings <- billings %>>%
+        dplyr::mutate(covid19bb = mapply(
+          check_for_covid19_bulkbilling,
+          InternalID, DOB, Date, Age, billings
+        ))
+
+      if (input$printcopy_view == TRUE) {
+        billings <- billings %>>%
+          dplyr::mutate(
+            billingtag_print = dplyr::if_else(
+              is.na(covid19bb),
+              billingtag_print,
+              paste(
+                billingtag_print,
+                paste0(", COVID-19 opportunity [", covid19bb, "]")
+              )
+            )
+          )
+      } else {
+        billings <- billings %>>%
+          dplyr::mutate(
+            billingtag = dplyr::if_else(
+              is.na(covid19bb),
+              billingtag,
+              paste0(
+                billingtag,
+                dMeasure::semantic_button(
+                  "COVID-19 BB",
+                  colour = "pink",
+                  popuphtml = paste0(
+                    "<p><font size = \'+0\'>",
+                    covid19bb,
+                    "</p>"
+                  )
+                )
+              )
+            )
+          )
+      }
+    }
+
     if (input$printcopy_view == TRUE) {
       # printable/copyable view
-      datatable_styled(billings_list() %>>%
+      datatable_styled(billings %>>%
         dplyr::select(
           Patient, Date, AppointmentTime, Status, VisitType,
           Provider, billingtag_print
@@ -107,7 +251,7 @@ billings_datatable <- function(input, output, session, dMBillings) {
       )
     } else {
       # fomantic/semantic tag view
-      datatable_styled(billings_list() %>%
+      datatable_styled(billings %>%
         dplyr::select(
           Patient, Date, AppointmentTime, Status, VisitType,
           Provider, billingtag
